dbfed9a2cb9668b6889f728d0ac02a3e756a4f0e
MadhuMishra663 <madhumishra663@gmail.com>
2024-05-10 22:07:49 +0530

    Admin

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,23 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom'; // Use Routes and Route, not Router
+// import React from 'react';
+import {  Routes, Route } from 'react-router-dom'; // Use Routes and Route, not Router
 import { Stays } from './pages/Stays';
 import { Experiences } from './pages/Experiences';
 import { OnlineExperiences } from './pages/OnlineExperiences';
 import { TopNavbar } from './components/topNavbar/TopNavbar';
+// import Admin from './components/admin/Admin';
+// import { Link } from 'react-router-dom';
 
 const App = () => {
   return (
     <>
       <TopNavbar />
+      {/* <Admin /> */}
+      
       <Routes>
         <Route path="/" element={<Stays />} />
         <Route path="/experiences" element={<Experiences />} />
         <Route path="/online-experiences" element={<OnlineExperiences />} />
+       
       </Routes>
     </>
   );
